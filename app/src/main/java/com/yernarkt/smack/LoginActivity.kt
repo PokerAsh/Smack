2680a2a20ca6aa278a3e849b26eb90378425e1d5
Yernar Mendigarin <yernar.m90@gmail.com>
2018-11-07 10:43:32 +0600

    SignUp Activity

--- app/src/main/java/com/yernarkt/smack/LoginActivity.kt
+++ app/src/main/java/com/yernarkt/smack/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.yernarkt.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -14,7 +15,8 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
-    fun loginCreateUserBtn(view: View){
-
+    fun loginCreateLoginBtnClick(view: View){
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/yernarkt/smack/SignUpActivity.kt
@@ -0,0 +1,25 @@
+package com.yernarkt.smack
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+
+    fun generateUserAvatar(view: View) {
+
+    }
+
+    fun generateColorClick(view: View) {
+
+    }
+
+    fun createUserBtnClick(view: View) {
+
+    }
+}
