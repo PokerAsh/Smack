bb8509ccf48647026013f399de4ea2ac8deb0922
Yernar Mendigarin <yernar.m90@gmail.com>
2018-11-07 11:01:48 +0600

    Generate Avatar and background color

--- app/src/main/java/com/yernarkt/smack/SignUpActivity.kt
+++ app/src/main/java/com/yernarkt/smack/SignUpActivity.kt
@@ -1,10 +1,16 @@
 package com.yernarkt.smack
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import java.util.*
 
 class SignUpActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +18,29 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        userAvatar = if (color == 0) "light$avatar" else "dark$avatar"
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+
+        createAvatarImageView.setImageResource(resourceId)
     }
 
     fun generateColorClick(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        createAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = r.toDouble() / 255
+        val savedB = r.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
     }
 
     fun createUserBtnClick(view: View) {
