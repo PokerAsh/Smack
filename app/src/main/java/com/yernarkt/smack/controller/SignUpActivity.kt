b8b3752746a0ec8aa78031a7a54e3f127f787ae1
Yernar Mendigarin <yernar.m90@gmail.com>
2018-11-07 16:46:46 +0600

    Polishing up sign up activity

--- app/src/main/java/com/yernarkt/smack/controller/SignUpActivity.kt
+++ app/src/main/java/com/yernarkt/smack/controller/SignUpActivity.kt
@@ -1,11 +1,16 @@
 package com.yernarkt.smack.controller
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.content.LocalBroadcastManager
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import com.yernarkt.smack.R
+import com.yernarkt.smack.util.BROADCAST_USER_DATA_CHANGE
 import com.yernarkt.smack.volley_network.AuthService
 import kotlinx.android.synthetic.main.activity_sign_up.*
 import java.util.*
@@ -17,6 +22,8 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        enableProgressBar(false)
     }
 
     fun generateUserAvatar(view: View) {
@@ -46,29 +53,58 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     fun createUserBtnClick(view: View) {
+        enableProgressBar(true)
         val userName = createUserNameText.text.toString()
         val email = createEmailText.text.toString()
         val password = createPasswordText.text.toString()
-//        val
 
-//        if (email != "" || password != "")
-        AuthService.registerUser(this, email, password) { registerSuccess ->
-            if (registerSuccess) {
-                AuthService.loginUser(this, email, password) { loginSuccess ->
-                    if (loginSuccess) {
-                        AuthService.createUser(this, userName, email, userAvatar, avatarColor){createUserSuccess ->
-                            if(createUserSuccess){
-                                finish()
+        if (email.isNotEmpty() || password.isNotEmpty() || userName.isNotEmpty())
+            AuthService.registerUser(this, email, password) { registerSuccess ->
+                if (registerSuccess) {
+                    AuthService.loginUser(this, email, password) { loginSuccess ->
+                        if (loginSuccess) {
+                            AuthService.createUser(
+                                this,
+                                userName,
+                                email,
+                                userAvatar,
+                                avatarColor
+                            ) { createUserSuccess ->
+                                if (createUserSuccess) {
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    enableProgressBar(false)
+                                    finish()
+                                } else {
+                                    errorSnackBar(view)
+                                }
                             }
+                        } else {
+                            errorSnackBar(view)
                         }
                     }
+                } else {
+                    errorSnackBar(view)
                 }
-            } else {
-                Snackbar.make(view, "Error", Snackbar.LENGTH_LONG).show()
             }
+        else
+            Snackbar.make(view, "Please enter username, email and password", Snackbar.LENGTH_LONG).show()
+    }
+
+    fun errorSnackBar(view: View) {
+        enableProgressBar(false)
+        Snackbar.make(view, "Something went wrong. Please try again", Snackbar.LENGTH_LONG).show()
+    }
+
+    fun enableProgressBar(enable: Boolean) {
+        if (enable) {
+            createPb.visibility = VISIBLE
+        } else {
+            createPb.visibility = GONE
         }
 
-//        else
-//            Snackbar.make(view, "Please enter email and password", Snackbar.LENGTH_LONG).show()
+        createUserBtn.isEnabled = !enable
+        createAvatarImageView.isEnabled = !enable
+        backgroundColorBtn.isEnabled = !enable
     }
 }
